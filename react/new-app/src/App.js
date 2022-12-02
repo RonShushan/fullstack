91f16188cff55954d1633029dd9eb8590ecbafba
ronshushan <ronshushan1@gmail.com>
2022-12-02 10:26:12 +0300

    REACT

---
+++ react/new-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+// import About from './pages/About';
+// import Cubes from './pages/Cubes/Cubes';
+import Home from './pages/Home/Home';
+
+
+
+
+function App() {
+
+  //js
+  return (
+    <div className="App">
+      {/* <Cubes /> */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/new-app/src/pages/Home/Home.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import CardComponent from '../../components/CardComponent';
+// import ShowComponent from '../../components/ShowComponent';
+import './Home.css'
+
+class Home extends Component {
+    //JS Start
+
+    state = {
+        name: "ron"
+    }
+
+
+    // constructor(props) {
+    //     super(props);
+    // }
+
+
+
+    //HTML Start
+    render() {
+        return (
+            <div className='p-5'>
+
+                <hr />
+                <div className='row'>
+                    <div className="col-3">
+                        <CardComponent />
+                    </div>
+                    <div className="col-3">
+                        <CardComponent />
+                    </div>
+                    <div className="col-3">
+                        <CardComponent />
+                    </div>
+                    <div className="col-3">
+                        <CardComponent />
+                    </div>
+                    <div className="col-3">
+                        <CardComponent />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
