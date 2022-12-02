826e279ae9f2d5dcec80300af1d9bc20d9955b10
ronshushan <ronshushan1@gmail.com>
2022-12-02 04:57:00 +0300

    new

--- javascript/AJAX API Project/project.js
+++ javascript/AJAX API Project/project.js
@@ -61,7 +61,7 @@ function navDV() {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="#" onclick="chartDIV()">Live Reports</a>
+                    <a class="nav-link" id="liveReports" href="#"" onclick="showChart()">Live Reports</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#" onclick="About()">About</a>
@@ -71,7 +71,7 @@ function navDV() {
             <form class="d-flex" role="search">
                 <input class="form-control me-2 coinInputDV" type="search" id="coinInputDV"
                     placeholder="Search" aria-label="Search">
-                <button class="btn btn-success coinInputDV" type="submit"
+                <button class="btn btn-success coinInputDV" type="button"
                     onclick="onSubmit(coinInputDV.value)">Search</button>
             </form>
         </div>
@@ -100,35 +100,10 @@ function printCoinsAll() {
 // }
 
 function printCoin(coin) {
+    let IdName = "cardID" + coin.id
     coinContainer.innerHTML += `
-        <div class="col-sm-12 col-md-4 col-lg-4 col-xl-3">
-        <div class="card" style='margin: 10px; margin-top: 70px; width:250px; height:auto;'>
-        <div class="card-body">
-        <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" role="switch" id="SwitchCheck">
-        <label class="form-check-label" for="flexSwitchCheckDefault"></label>
-        <div class="card-title" id="cardTitle">${coin.symbol} <img class="card-img" id="coinImg" src="${coin.image.thumb}" style="width:50px; height:50px;">
-        </div>
-        </div>
-        <div>
-        <div class="card-text">${coin.name}</div><br></div>
-        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample">more info</button>
-        <div class="collapse" id="collapseExample"><br>
-        <img class="card-img" id="coinImg" src="${coin.image.thumb}" style="float:right; width:70px; height:70px;">
-        <div><strong>USD :</strong> ${coin.market_data.current_price.usd} $</div>
-        <div><strong>EUR :</strong> ${coin.market_data.current_price.eur} €</div>
-        <div><strong>ILS :</strong> ${coin.market_data.current_price.ils} ₪</div>
-        </div>
-        </div>
-        </div>
-        </div> 
-    `
-}
-
-function printSingleCoin(coin) {
-    coinContainer.innerHTML = `
     <div class="col-sm-12 col-md-4 col-lg-4 col-xl-3">
-    <div class="card" style='margin: 10px; margin-top: 70px; width:250px; height:auto;'>
+    <div class="card">
     <div class="card-body">
     <div class="form-check form-switch">
     <input class="form-check-input" type="checkbox" role="switch" id="SwitchCheck">
@@ -138,8 +113,8 @@ function printSingleCoin(coin) {
     </div>
     <div>
     <div class="card-text">${coin.name}</div><br>
-    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample">more info</button>
-    <div class="collapse" id="collapseExample"><br>
+    <button class="btn btn-primary" type="" data-bs-toggle="collapse" >more info</button>
+    <div class="collapse" id="${IdName}"><br>
     <img class="card-img" id="coinImg" src="${coin.image.thumb}" style="float:right; width:70px; height:70px;">
     <div><strong>USD :</strong> ${coin.market_data.current_price.usd} $</div>
     <div><strong>EUR :</strong> ${coin.market_data.current_price.eur} €</div>
@@ -151,6 +126,39 @@ function printSingleCoin(coin) {
 `
 }
 
+
+function printSingleCoin(coin) {
+    let IdName = "cardID" + coin.id
+    let index = coin.name
+    coinContainer.innerHTML = `
+    <div class="col-sm-12 col-md-4 col-lg-4 col-xl-3">
+    <div class="card">
+    <div class="card-body">
+    <div class="form-check form-switch">
+    <input class="form-check-input" type="checkbox" role="switch" id="SwitchCheck" onchange="toggleSelect()">
+    <label class="form-check-label" for="flexSwitchCheckDefault"></label>
+    <div class="card-title" id="cardTitle">${coin.symbol} <img class="card-img" id="coinImg" src="${coin.image.thumb}" style="width:50px; height:50px;">
+    </div>
+    </div>
+    <div>
+    <div class="card-text">${coin.name}</div><br>
+    <button class="btn btn-primary" type="" data-bs-toggle="collapse" data-bs-target="#${IdName}">more info</button>
+    <div class="collapse" id="${IdName}"><br>
+    <img class="card-img" id="coinImg" src="${coin.image.thumb}" style="float:right; width:70px; height:70px;">
+    <div><strong>USD :</strong> ${coin.market_data.current_price.usd} $</div>
+    <div><strong>EUR :</strong> ${coin.market_data.current_price.eur} €</div>
+    <div><strong>ILS :</strong> ${coin.market_data.current_price.ils} ₪</div>
+    </div>
+    </div>
+    </div>
+    </div> 
+`
+}
+
+
+
+
+
 function About() {
     coinContainer.innerHTML = `
     <div id="AboutInfo" >    
@@ -160,5 +168,94 @@ function About() {
     `
 
 }
+
+function showChart() {
+    var chart = new CanvasJS.Chart('coinContainer', {
+        animationEnabled: true,
+        title: {
+            text: 'My Coins',
+        },
+        axisX: {
+            valueFormatString: 'DD MMM,YY',
+        },
+        axisY: {
+            title: 'Coin value',
+            suffix: '$',
+        },
+        legend: {
+            cursor: 'pointer',
+            fontSize: 16,
+            itemclick: toggleDataSeries,
+        },
+        toolTip: {
+            shared: true,
+        },
+        data: [{
+            name: 'OB1',
+            type: 'spline',
+            yValueFormatString: '#0.## $',
+            showInLegend: true,
+            dataPoints: [{
+                x: new Date(2017, 6, 4),
+                y: 1800
+            }, {
+                x: new Date(2017, 6, 5),
+                y: 1800
+            }, {
+                x: new Date(2017, 6, 6),
+                y: 1825
+            }, {
+                x: new Date(2017, 6, 7),
+                y: 1827
+            }, {
+                x: new Date(2017, 6, 8),
+                y: 1835
+            }, {
+                x: new Date(2017, 6, 9),
+                y: 1845
+            }, {
+                x: new Date(2017, 6, 10),
+                y: 1857
+            },],
+        },],
+    })
+    chart.render()
+    addToGraph()
+
+    function toggleDataSeries(e) {
+        if (
+            typeof e.dataSeries.visible === 'undefined' ||
+            e.dataSeries.visible
+        ) {
+            e.dataSeries.visible = false
+        } else {
+            e.dataSeries.visible = true
+        }
+        chart.render()
+    }
+
+    function addToGraph() {
+        let Y = 1900
+        let Day = 11
+
+        setInterval(function () {
+            var date = new Date()
+            var minutes = date.getMinutes()
+            var hour = date.getHours()
+
+            let ob = {
+                x: new Date(2017, 6, Day),
+                y: Y,
+            }
+
+            chart.data[0].dataPoints.push(ob)
+            chart.render()
+            Y += 100
+            Day++
+            console.log('C : ', chart.data[0].dataPoints)
+        }, 2000)
+    }
+}
+
 
 
